da727ff865c547b8ca68981b7268ba59f65c968b
lizzy gao <yu.gao@nagra.com>
2016-05-20 10:48:29 +0800

    change webkit testharness to register a window /complete_callback/ handler

--- wptrunner/executors/testharness_webdriver.js
+++ wptrunner/executors/testharness_webdriver.js
@@ -5,6 +5,13 @@
 var callback = arguments[arguments.length - 1];
 window.timeout_multiplier = %(timeout_multiplier)d;
 
+/** Change the event handler
+ ** Github source: use the window.addEventListener("message", function(event){}
+ **                it can't get the message from the window created by window.open()
+ **                see the https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage 
+ ** Change to:     window.completion_callback(), which is refer to the current web-platform.test
+ **                see web-platform\tools\runner\runner.js
+ 
 window.addEventListener("message", function(event) {
   var tests = event.data[0];
   var status = event.data[1];
@@ -20,6 +27,21 @@ window.addEventListener("message", function(event) {
             status.stack,
             subtest_results]);
 }, false);
+
+*/
+window.completion_callback = function(tests, status) {
+    var subtest_results = tests.map(function (test) {
+            return [test.name, test.status, test.message, test.stack]
+        });
+    
+    clearTimeout(timer);
+    callback(["%(url)s",
+        status.status,
+        status.message,
+        status.stack,
+        subtest_results]);
+};
+
 
 window.win = window.open("%(abs_url)s", "%(window_id)s");
 
